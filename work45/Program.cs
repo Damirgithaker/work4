ccc11f8bca23533b9023cbd414e1ed4ea453823f
Dmitry <damir@gmail.com>
2022-07-24 20:50:01 +0300

    Решены задачи 47 и 50

---
+++ work45/Program.cs
@@ -0,0 +1,45 @@
+﻿
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+
+
+int[,] myArray = new int[3, 5];
+Random rnd = new Random();
+
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        myArray[i, j] = rnd.Next(0, 10);
+        Console.Write(myArray[i, j]);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+
+void CreateArrayRndDouble(int rows, int col)
+{
+
+    if (rows > (myArray.GetLength(0)) || col > (myArray.GetLength(1)))
+    {
+        Console.WriteLine("Индексы вышли за границы!");
+    }
+    else
+    {
+
+        Console.Write(myArray[rows - 1, col - 1]);
+    }
+Console.WriteLine();
+}
+Console.Write("Введите позицию строки - ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позтцию столбца - ");
+int col = Convert.ToInt32(Console.ReadLine());
+CreateArrayRndDouble(rows, col);
+
---
+++ work46/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+Console.WriteLine("Введите количество строк ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество стоблцов ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] myArray = new double[m, n];
+Random rnd = new Random();
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        myArray[i, j] = Math.Round((rnd.NextDouble() * (100 - 4) + 4), 2);
+        Console.Write($"{myArray[i, j]} ");
+        
+    }
+Console.WriteLine();
+}
